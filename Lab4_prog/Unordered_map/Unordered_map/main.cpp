100c2b46800dd9a158f60cbc16df373f6b0477b1
Nikita <nikita.malewich@gmail.com>
2019-04-03 00:49:02 +0300

    Added Lab4_prog

--- Lab4_prog/Unordered_map/Unordered_map/main.cpp
+++ Lab4_prog/Unordered_map/Unordered_map/main.cpp
@@ -6,22 +6,7 @@ int main()
 {
 	Unordered_map<int, const char*> my_table(10);
 
-	my_table.insert(8, "Nikita");
-	my_table.insert(19, "Anna");
-	my_table.insert(17, "Andrei");
-	my_table.insert(23, "Tanya");
-	my_table.insert(18, "Masha");
-	my_table.insert(3, "Nastya");
-	my_table.show();
-	std::cout << my_table.count() << std::endl;
-	std::cout << my_table.size() << std::endl;
-	std::cout << my_table.empty() << std::endl;
-	my_table.clear();
-	my_table.show();
-	std::cout << my_table.count() << std::endl;
-	std::cout << my_table.size() << std::endl;
-	std::cout << my_table.empty() << std::endl;
-	
+	my_table.test();
 
 	system("pause");
 	return 0;
--- Lab4_prog/Unordered_map/Unordered_map/unordered_map.h
+++ Lab4_prog/Unordered_map/Unordered_map/unordered_map.h
@@ -1,35 +1,192 @@
 #pragma once
 #include <iostream>
 #include <optional>
+#include <filesystem>
 
 template <typename TKey, typename TValue>
 struct node
 {
 	TKey key;
 	TValue value;
+public:
+	node<TKey, TValue>() = default;
+	node<TKey, TValue>& operator= (std::optional<node<TKey, TValue>>& a);
 };
 
 template <typename TKey, typename TValue>
+node<TKey, TValue>& node<TKey, TValue>::operator= (std::optional<node<TKey, TValue>>& a)
+{
+	this->key = a->key;
+	this->value = a->value;
+	return *this;
+}
+
+template <typename TKey, typename TValue>
 class Unordered_map {
-	std::optional<node<TKey, TValue>> *table_;
+	std::optional<node<TKey, TValue>>* table_;
 	unsigned capacity_;
 	unsigned count_;
 
 public:
+	class iterator;
 	Unordered_map();
 	Unordered_map(unsigned capasity);
 	~Unordered_map();
-	unsigned hash(int key) const;
+	unsigned hash(TKey key) const;
 	TValue search(TKey key) const;
 	void insert(TKey key_val, TValue el_value);
 	void show() const;
 	void Delete(TKey key);
-	TKey operator[] (unsigned position) const;
+	TKey& operator[] (unsigned position) const;
 	unsigned count() const;
 	unsigned size() const;
 	bool empty() const;
 	void clear();
+	iterator begin();
+	iterator end();
+	void test();
+
+	friend class iterator;
+
+	class iterator
+	{
+		std::optional<node<TKey, TValue>>* current_;
+	public:
+		iterator(std::optional<node<TKey, TValue>>* first);
+		std::optional<node<TKey, TValue>>& operator+ (int n);
+		std::optional<node<TKey, TValue>>& operator- (int n);
+		std::optional<node<TKey, TValue>>& operator++ (int);
+		std::optional<node<TKey, TValue>>& operator-- (int);
+		bool operator!= (const iterator& it);
+		bool operator== (const iterator& it);
+		std::optional<node<TKey, TValue>>& operator* ();
+	};
 };
+
+template <typename TKey, typename TValue>
+Unordered_map<TKey, TValue>::iterator::iterator (std::optional<node<TKey, TValue>>* first)
+{
+	current_ = first;
+}
+
+template <typename TKey, typename TValue>
+std::optional<node<TKey, TValue>>& Unordered_map<TKey, TValue>::iterator::operator+ (int n)
+{
+	while (n > 0)
+	{
+		current_ += 1;
+		if (*current_ != std::nullopt)
+		{
+			n--;
+		}
+	}
+	return *current_;
+}
+
+template <typename TKey, typename TValue>
+std::optional<node<TKey, TValue>>& Unordered_map<TKey, TValue>::iterator::operator- (int n)
+{
+	while (n > 0)
+	{
+		current_ -= 1;
+		if (*current_ != std::nullopt)
+		{
+			n--;
+		}
+	}
+	return *current_;
+}
+
+template <typename TKey, typename TValue>
+std::optional<node<TKey, TValue>>& Unordered_map<TKey, TValue>::iterator::operator++ (int)
+{
+	while (true)
+	{
+		if (*(current_ + 1) != std::nullopt)
+		{
+			return *current_++;
+		}
+		current_ += 1;
+	}
+}
+
+template <typename TKey, typename TValue>
+std::optional<node<TKey, TValue>>& Unordered_map<TKey, TValue>::iterator::operator-- (int)
+{
+	while (true)
+	{
+		if (*(current_ - 1) != std::nullopt)
+		{
+			return *current_--;
+		}
+		current_ -= 1;
+	}
+}
+
+template <typename TKey, typename TValue>
+bool Unordered_map<TKey, TValue>::iterator::operator!=(const iterator& it)
+{
+	return current_ != it.current_;
+}
+
+template <typename TKey, typename TValue>
+bool Unordered_map<TKey, TValue>::iterator::operator==(const iterator& it)
+{
+	return this.current_ == it.current_;
+}
+
+
+template <typename TKey, typename TValue>
+std::optional<node<TKey, TValue>>& Unordered_map<TKey, TValue>::iterator::operator* ()
+{
+	return *current_;
+}
+
+template <typename TKey, typename TValue>
+typename Unordered_map<TKey, TValue>::iterator Unordered_map<TKey, TValue>::begin()
+{
+	try
+	{
+		if (this->empty())
+		{
+			throw std::exception("Container is empty!");
+		}
+		for (int i = 0; i < capacity_; i++)
+		{
+			if (table_[i] != std::nullopt)
+			{
+				return table_ + i;
+			}
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
+
+template <typename TKey, typename TValue>
+typename Unordered_map<TKey, TValue>::iterator Unordered_map<TKey, TValue>::end()
+{
+	try
+	{
+		if (this->empty())
+		{
+			throw std::exception("Container is empty!");
+		}
+		for (int i = capacity_; i >= 0; i--)
+		{
+			if (table_[i] != std::nullopt)
+			{
+				return table_ + i;
+			}
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
 
 template <typename TKey, typename TValue>
 Unordered_map<TKey, TValue>::Unordered_map()
@@ -55,9 +212,10 @@ Unordered_map<TKey, TValue>::~Unordered_map<TKey, TValue>()
 }
 
 template <typename TKey, typename TValue>
-unsigned Unordered_map<TKey, TValue>::hash(int const key) const
+unsigned Unordered_map<TKey, TValue>::hash(TKey key) const
 {
-	return key % capacity_;
+	auto const const_hash = 0.618033;
+	return capacity_ * fmod(int(key) * const_hash, 1);
 }
 
 template <typename TKey, typename TValue>
@@ -71,20 +229,8 @@ void Unordered_map<TKey, TValue>::insert(TKey key_val, TValue el_value) {
 				throw std::exception("An object with this key already exist!");
 			}
 		}
-		unsigned position = hash((int)key_val);
-		for (int i = position; i < capacity_; i++)
-		{
-			if (table_[i] == std::nullopt)
-			{
-				node<TKey, TValue> a;
-				table_[i] = a;
-				table_[i]->key = key_val;
-				table_[i]->value = el_value;
-				break;
-			}
-		}
-		count_++;
-		if (count_ * 3 >= capacity_)
+		unsigned const position = hash((int)key_val);
+		if (count_ * 2 >= capacity_)
 		{
 			capacity_ *= 2;
 			std::optional<node<TKey, TValue>> *new_table = new std::optional<node<TKey, TValue>>[capacity_];
@@ -101,6 +247,35 @@ void Unordered_map<TKey, TValue>::insert(TKey key_val, TValue el_value) {
 			delete[] table_;
 			table_ = new_table;
 		}
+		if (position >= capacity_)
+		{
+			capacity_ = position + 1;
+			std::optional<node<TKey, TValue>> *new_table = new std::optional<node<TKey, TValue>>[capacity_];
+			for (int i = 0; i < capacity_ / 2; i++)
+			{
+				if (table_[i] != std::nullopt)
+				{
+					node<TKey, TValue> a;
+					new_table[i] = a;
+					new_table[i]->key = table_[i]->key;
+					new_table[i]->value = table_[i]->value;
+				}
+			}
+			delete[] table_;
+			table_ = new_table;
+		}
+		for (int i = position; i < capacity_; i++)
+		{
+			if (table_[i] == std::nullopt)
+			{
+				node<TKey, TValue> a;
+				table_[i] = a;
+				table_[i]->key = key_val;
+				table_[i]->value = el_value;
+				break;
+			}
+		}
+		count_++;
 	}
 	catch (std::exception& e)
 	{
@@ -150,6 +325,7 @@ void Unordered_map<TKey, TValue>::Delete(TKey key)
 			if (table_[i] != std::nullopt && table_[i]->key == key)
 			{
 				table_[i] = std::nullopt;
+				count_--;
 				return;
 			}
 		}
@@ -162,7 +338,7 @@ void Unordered_map<TKey, TValue>::Delete(TKey key)
 }
 
 template <typename TKey, typename TValue>
-TKey Unordered_map<TKey, TValue>::operator[](unsigned position) const
+TKey& Unordered_map<TKey, TValue>::operator[](unsigned position) const
 {
 	try
 	{
@@ -211,4 +387,97 @@ void Unordered_map<TKey, TValue>::clear()
 	{
 		table_[i] = std::nullopt;
 	}
+}
+
+template <typename TKey, typename TValue>
+void Unordered_map<TKey, TValue>::test()
+{
+	this->insert(14, "Nikita");
+	this->insert(2481, "Anna");
+	this->insert(35, "Andrei");
+	this->insert(124, "Tanya");
+	this->insert(52, "Masha");
+	this->insert(642, "Nastya");
+
+	this->show();
+	std::cout << std::endl;
+
+	this->Delete(52);
+
+	for (auto it = this->begin(); it != this->end(); it++)
+	{
+		node<TKey, TValue> a;
+		a = *it;
+		std::cout << a.key << ": " << a.value << std::endl;
+	}
+	std::cout << std::endl;
+
+	if (this->empty())
+	{
+		std::cout << "Container is empty!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Container isn't empty! Count = " << this->count() << ". Size = " << this->size() << std::endl;
+	}
+	std::cout << std::endl;
+
+	this->clear();
+
+	if (this->empty())
+	{
+		std::cout << "Container is empty!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Container isn't empty! Count = " << this->count() << ". Size = " << this->size() << std::endl;
+	}
+	std::cout << std::endl;
+
+	this->insert(89, "Petya");
+	this->insert(775, "Vasya");
+	this->insert(2000, "Sasha");
+	this->insert(1991, "Kolya");
+	this->insert(2323, "Masha");
+	this->insert(688, "Nastya");
+	this->insert(17, "Nikita");
+	this->insert(2600, "Anna");
+	this->insert(3000, "Andrei");
+	this->insert(41, "Tanya");
+	this->insert(1392, "Volodya");
+	this->insert(72, "Seva");
+	this->insert(124, "Denis");
+	this->insert(100, "Vlad");
+	this->insert(9999, "Egor");
+	this->insert(12114, "Stas");
+	this->insert(70, "Dasha");
+	this->insert(3872, "Arina");
+	this->insert(16, "Violetta");
+	this->insert(248781, "Lesha");
+	this->insert(352, "Aleksey");
+	this->insert(12489, "Ilya");
+	this->insert(2, "Anastasia");
+	this->insert(567, "Igor");
+	this->insert(14, "Maya");
+	this->insert(2481, "Kostya");
+	this->insert(35, "Misha");
+	this->insert(124, "Somar");
+	this->insert(52, "Maksim");
+	this->insert(642, "Vladislav");
+
+	this->show();
+	std::cout << std::endl;
+
+	if (this->empty())
+	{
+		std::cout << "Container is empty!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Container isn't empty! Count = " << this->count() << ". Size = " << this->size() << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << this->search(124) << std::endl;
+	std::cout << std::endl;
 }
